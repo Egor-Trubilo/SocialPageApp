104f09b679c0ae41347776bb68ffec594f093238
Egor_Trubilo <trubilooficial@gmail.com>
2021-10-30 17:46:55 +0300

    add Class component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,63 +1,40 @@
 import React from 'react';
 import s from './users.module.css';
 import * as axios from "axios";
+import userPhoto from '../../Assets/images/grapefruit-slice-332-332.jpg';
 
+class Users extends React.Component {
 
-    let Users = (props) => {
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
 
-        if (props.users.length === 0) {
-
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-
-                props.setUsers(response.data.items);
-            });
-
-            props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
-                    followed: true,
-                    fullName: 'Egor',
-                    status: 'i am the best',
-                    location: {city: 'Kalchi', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
-                    followed: true,
-                    fullName: 'Sergey',
-                    status: 'gigi',
-                    location: {city: 'Mozyr', country: 'Russian Federation'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
-                    followed: false,
-                    fullName: 'Natallia',
-                    status: 'pfff',
-                    location: {city: 'Gomel', country: 'Belarus'}
-                },
-            ]
-
-        )
-
+            this.props.setUsers(response.data.items);
+        })
     }
 
+
+render()
+{
     return <div>
-            {
-                props.users.map(u => <div key={u.id}>
+        {
+            this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
-                        <img className={s.userPhoto} src={u.photoUrl} />
+                        <img className={s.userPhoto} src={u.photos.small != null ? u.photos.small : userPhoto}/>
                     </div>
                     <div>
                         {u.followed
-                            ?  <button onClick={() => {props.unfollow(u.id)}}> Unfollow </button>
-                            : <button onClick={() => {props.follow(u.id)}}>Follow</button> }
+                            ? <button onClick={() => {
+                                this.props.unfollow(u.id)
+                            }}> Unfollow </button>
+                            : <button onClick={() => {
+                                this.props.follow(u.id)
+                            }}>Follow</button>}
 
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>
                             {u.name}
@@ -75,9 +52,10 @@ import * as axios from "axios";
                         </div>
                     </span>
                 </span>
-            </div>)}
+                </div>)}
         </div>
-
+    }
 }
+   
 
-export default Users;
+export  default Users;
